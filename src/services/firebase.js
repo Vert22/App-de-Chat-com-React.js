36ed673cbcd522b52f81d3a9dca63d295303b15e
Vert22 <vert22022@gmail.com>
2022-10-26 03:06:57 -0300

    1.1

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,12 +3,12 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyA3IOrRWji1CI1woI1IS56h8n7CjUJDYd0",
+  authDomain: "chat-app-c29ea.firebaseapp.com",
+  projectId: "chat-app-c29ea",
+  storageBucket: "chat-app-c29ea.appspot.com",
+  messagingSenderId: "80717713763",
+  appId: "1:80717713763:web:22fe2fa2f30301fa204356"
 };
 
 const app = firebase.initializeApp(firebaseConfig);
