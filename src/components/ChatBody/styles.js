12a8b318d80924ebb7f0f1ac1ff25b1f8ddbc1a8
Vert22 <vert22022@gmail.com>
2022-10-26 03:36:13 -0300

    1.2

--- src/components/ChatBody/styles.js
+++ src/components/ChatBody/styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   flex: 1;
-  background-color: #efeae2;
+  background-color: rgb(50, 50, 50);
   overflow-y: auto;
 
   &::-webkit-scrollbar {
--- src/components/Login/styles.js
+++ src/components/Login/styles.js
@@ -5,6 +5,7 @@ export const Container = styled.div`
   height: 100vh;
   align-items: center;
   justify-content: center;
+  background-color: rgb(50, 50, 50);
 `;
 
 export const Button = styled.button`
@@ -12,4 +13,5 @@ export const Button = styled.button`
   font-size: 18px;
   padding: 14px 18px;
   cursor: pointer;
+  color: aqua;
 `;
